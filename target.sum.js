6d20e71b4e8df0399be34c176039c20e9aaf9f9a
gdanis <gustavodsf1@gmail.com>
2024-05-30 16:30:10 -0300

    sum of two numbers

--- target.sum.js
+++ target.sum.js
@@ -6,11 +6,61 @@
 //   Note that the target sum has to be obtained by summing two different integers
 //   in the array; you can't add a single integer to itself in orde
 
+//Naive
 function twoNumberSum(array, targetSum) {
-    return 0;
+    sums = [];
+    for(let i = 0; i < array.length ; i++){
+        for( let j = i + 1; j < array.length; j++){
+            if(array[i] + array[j] === targetSum){
+                sums.push([array[i], array[j]])
+            }
+        }
+    }
+    return sums
 }
 
-const array = [3, 5, -4, 8, 11, 1, -1, 6];
-const targetSum = 10;
 
-console.log(twoNumberSum(array, targetSum));
+//faster solution
+function twoNumberSum(array, targetSum) {
+    const sums = [];
+    const hashTable = {}
+  
+    for(let i = 0; i < array.length ; i++){
+       const sumMinusElement = targetSum - array[i];
+       if( hashTable[sumMinusElement.toString()] !== undefined ) {
+         sums.push([array[i], sumMinusElement])
+       }
+      
+      hashTable[array[i].toString()] = array[i]
+      
+    }
+    return sums
+}
+
+function twoNumberSum(array, targetSum) {
+    const sorted = array.sort((a,b) => a - b)
+    let left = 0
+    let right = sorted.length - 1
+    console.log(sorted)
+    
+    while(left < right){
+      const currentSum = sorted[left] + sorted[right]
+      console.log([sorted[left] , sorted[right]])
+      if(currentSum > targetSum){
+        right = right - 1
+      } else if(currentSum < targetSum){
+        left = left + 1
+      } else {
+        return [
+          sorted[left] , sorted[right]
+        ]
+      }
+    }
+      
+    const sums = []
+   
+    return sums
+  }
+
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
+console.log(twoNumberSum([3, 5, 2, -4, 8, 11], 7));       
