1c6fe7c8aff99b994b6d3db2480ab64de41290b7
gdanis <gustavodsf1@gmail.com>
2024-06-19 22:44:06 -0300

    binary search

---
+++ binary_search_smart.js
@@ -0,0 +1,26 @@
+const binarySearch = (array, target) => {
+ const middle = Math.floor(array.length/2)
+ 
+ let lowerPointer = 0
+ let highestPointer = middle
+ 
+ if(array[middle] < target) {
+    lowerPointer = middle
+    highestPointer = array.length-1
+ }
+ 
+ for(let i = lowerPointer; i <= highestPointer; i++){
+    if(array[lowerPointer] === target){
+      return lowerPointer
+    }
+    if(array[highestPointer] === target){
+      return highestPointer
+    }
+   lowerPointer++
+   highestPointer--
+ }
+ 
+ return -1
+}
+
+binarySearch([1,2,3,4,5,6,7,8, 9], 7)
